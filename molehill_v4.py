8457d72eb5432c1ea0c2d78fcd20f519a1c2950c
Sean CC <seanlongcc@gmail.com>
2022-02-12 14:54:44 -0500

    +select columns for WA +contact table

--- molehill_v4.py
+++ molehill_v4.py
@@ -49,7 +49,7 @@ def tabLayout():
                 #organize the tabs
                 tabControl.pack(expand = True, fill ="both")
 
-                #txt
+                #text files
                 if name.lower().endswith('.txt'):
                     #put a scrolled text box onto the tab and have it fill the area
                     content = st.ScrolledText(tab, wrap = tk.WORD)
@@ -67,7 +67,13 @@ def tabLayout():
                     try:
                         filepath = db_to_csv(name)
                     except:
-                        filepath = db_to_csv2(name)
+                        try:
+                            filepath = db_to_csv2(name)
+                        except:
+                            try:
+                                filepath = display_idName(name)
+                            except:
+                                print("database function does not exist")
 
                     #display the csv
                     csvDisplay(filepath, tab)
@@ -95,9 +101,27 @@ def db_to_csv(name, tableName = "messages"):
 
     #connect with the database
     con = sqlite3.connect(db_path)
+    con_wa = sqlite3.connect(os.path.join(BASE_DIR, "wa.db"))
 
     #read the database file
-    df = pd.read_sql_query("SELECT * FROM {}".format(tableName), con)
+    df = pd.read_sql_query("SELECT _id, key_remote_jid, key_from_me, data, timestamp, media_url, media_mime_type, media_size, received_timestamp, receipt_server_timestamp FROM {}".format(tableName), con)
+
+    #read database file of wa to get contacts
+    df_wa = pd.read_sql_query("SELECT jid, number, display_name FROM wa_contacts", con_wa)
+
+    wa_dict = {}
+    #dictionary of contacts in wa, maps jid to display name
+    for i in range(len(df_wa)):
+       wa_dict[df_wa.iloc[i]["jid"]] = df_wa.iloc[i]["display_name"]
+
+    match = []
+    #corresponds mgstore and wa, maps phone number, and display name
+    for i in range(1, len(df)):
+        if df.iloc[i]["key_from_me"] == 0:
+            match.append([wa_dict[df.iloc[i]["key_remote_jid"]], df.iloc[i]["data"]])
+        else:
+            match.append(["FROM ME", df.iloc[i]["data"]])
+    print(match)
 
     #convert db to csv, is not a string
     df.to_csv(r'{}.csv'.format(name[:-3]), index = False)
@@ -107,6 +131,63 @@ def db_to_csv(name, tableName = "messages"):
     return filepath
 
 def db_to_csv2(name, tableName = "wa_contacts"):
+    #set the directory of the file to our current directory
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, name)
+
+    #connect with the database
+    con = sqlite3.connect(db_path)
+
+    #read the database file
+    df = pd.read_sql_query("SELECT _id, jid, status, number, display_name, given_name, sort_name, nickname FROM {}".format(tableName), con)
+
+    #convert db to csv, is not a string
+    df.to_csv(r'{}.csv'.format(name[:-3]), index = False)
+
+    #set filepath name
+    filepath = name[:-3] + ".csv"
+    return filepath
+
+def idName(name, tableName = "id_and_name"):
+    #set the directory of the file to our current directory
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, name)
+
+    #connect with the database
+    con = sqlite3.connect(db_path)
+    con_wa = sqlite3.connect(os.path.join(BASE_DIR, "wa.db"))
+    conID = sqlite3.connect('id_and_name.db')
+
+    #read the database file
+    df = pd.read_sql_query("SELECT * FROM {}".format(tableName), con)
+
+    #read database file of wa to get contacts
+    df_wa = pd.read_sql_query("SELECT jid, display_name FROM wa_contacts", con_wa)
+
+    #make cursors for each database
+    cur = conID.cursor()
+    curWA = con_wa.cursor()
+
+    #get the data from wa_contacts and puts into rows
+    curWA.execute("SELECT jid, display_name FROM wa_contacts")
+    rows = curWA.fetchall()
+
+    #put data from rows into id_and_name
+    sql_statement = 'INSERT INTO id_and_name VALUES (?, ?)'
+    cur.executemany(sql_statement, rows)
+    
+    #commit and close the db
+    conID.commit()
+    conID.close()
+
+    #convert db to csv, is not a string
+    df.to_csv(r'{}.csv'.format(name[:-3]), index = False)
+
+    #set filepath name
+    filepath = name[:-3] + ".csv"
+    return filepath
+
+def display_idName(name, tableName = "id_and_name"):
     #set the directory of the file to our current directory
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     db_path = os.path.join(BASE_DIR, name)
@@ -131,9 +212,19 @@ def csvDisplay(filepath, tab):
 
     #open the csv file
     table.importCSV(filepath)
-
+    
     #show the csv file on the table
     table.show()
+
+#create new database for ID and Name
+con = sqlite3.connect('id_and_name.db')
+cur = con.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS id_and_name (ID, Name)''')
+idName('id_and_name.db')
+#cur.execute("INSERT INTO id_and_name VALUES (100,100)")
+con.commit()
+con.close()
 
 #set for uploadedFiles
 uploadedFiles = set()
